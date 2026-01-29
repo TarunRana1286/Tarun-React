513b63fbe6f2c9238a3878cd3f9f2fe440eada8a
TarunRana1286 <tarunrana237h@gmail.com>
2026-01-29 08:31:59 +0530

    With MyApp function in Main.jsx

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+//import { createRoot }  from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+function MyApp() {
+  return <div> <h1>Hello Vite + React!</h1></div>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MyApp />
   </StrictMode>,
 )
