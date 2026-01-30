b9052bd17bc0cc68a69f9723810c10a1f1e80e8d
TarunRana1286 <tarunrana237h@gmail.com>
2026-01-31 00:41:14 +0530

    New Project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import {useState} from 'react'
+import './App.css'
+
+function App() {
+ 
+  let [counter, setCounter] = useState(7);
+  ///let counter =7;
+
+  const handleAdd = () => {
+    setCounter(counter + 1);
+    console.log("Add button clicked", counter);
+  }
+
+  const handleRemove = () => {
+    setCounter(counter - 1);
+    console.log("Remove button clicked", counter);
+  } 
+
+  return (
+    <>
+      <h1>Tarun React Apppppp</h1>
+      <h2>Counter Value : {counter}</h2>
+
+      <button onClick={handleAdd}>Add Value {counter}</button>
+      <br />
+      <button onClick={handleRemove}>Remove Value {counter}</button>
+      <br />
+      <footer>Footer : {counter}</footer>
+    </>
+  )
+}
+
+export default App
