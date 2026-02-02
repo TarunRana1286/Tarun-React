757310a26949b060f831b5426287902d90bc72d8
TarunRana1286 <tarunrana237h@gmail.com>
2026-02-02 23:56:42 +0530

    My Changes

---
+++ 04tailwindprops/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello Tailwind CSS!</h1>
+    </>
+  )
+}
+
+export default App
