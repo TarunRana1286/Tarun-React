74726d2e3f182748d26475f2bd0faf0dc8608865
TarunRana1286 <tarunrana237h@gmail.com>
2026-02-03 09:45:55 +0530

    props start

--- 05tailwindprops/src/App.jsx
+++ 05tailwindprops/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import React from "react"
+import Card from '../components/card'
 
 function App() {
 
   return (
     <>
     <h1 className=" bg-green-500 rounded-xl text-3xl font-bold"> Tarun Rana Tailwind </h1>
+    <br></br>
+    <Card></Card>
+    <Card></Card>
+
     </>
   )
 }
