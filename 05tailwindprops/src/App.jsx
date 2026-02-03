a8f7b85a18db700aa33292dda4f8c25fbc4ed660
TarunRana1286 <tarunrana237h@gmail.com>
2026-02-03 23:50:28 +0530

    props changes with btn

--- 05tailwindprops/src/App.jsx
+++ 05tailwindprops/src/App.jsx
@@ -4,12 +4,18 @@ import Card from '../components/card'
 
 function App() {
 
+  let myobj = {
+    name: "Tarun Rana",
+    age: 24,
+    city: "New Delhi"
+  }
+
   return (
     <>
     <h1 className=" bg-green-500 rounded-xl text-3xl font-bold"> Tarun Rana Tailwind </h1>
     <br></br>
-    <Card></Card>
-    <Card></Card>
+    <Card username="Tarun Rana Tailwind" btnText="View Profile"></Card>
+    <Card city="Ahmedabad"></Card>
 
     </>
   )
