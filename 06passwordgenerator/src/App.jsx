66dab87ba8f923cc1d9159011852a7a2429f3f79
TarunRana1286 <tarunrana237h@gmail.com>
2026-02-10 09:25:36 +0530

    Start

--- 06passwordgenerator/src/App.jsx
+++ 06passwordgenerator/src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react"
 import './App.css'
 import React from "react"
 
 function App() {
 
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
   return (
     <>
-      <h1>New Password</h1>
+      <h1>Password Generator</h1>
     </>
   )
 }
