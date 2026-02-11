4b34becf3e3cb596c518e05afed9cbfdafbaae16
TarunRana1286 <tarunrana237h@gmail.com>
2026-02-11 09:39:30 +0530

    Custom Hooks Creation

---
+++ currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,17 @@
+import  {use, useeffect, useState} from 'react'
+
+function useCurrencyInfo(currency) {  
+
+    useEffect(() => {
+        fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
+            .then(response => response.json())
+            .then(data => {
+                setCurrencyInfo(data);
+            })
+            .catch(error => {
+                console.error('Error fetching currency data:', error);
+            });
+    }, [currency]);
+
+    const [currencyInfo, setCurrencyInfo] = useState(null); 
+ }
