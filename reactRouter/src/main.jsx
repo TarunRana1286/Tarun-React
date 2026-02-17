d35e22e56fc24b8f5103b98e36795400ee624d8e
TarunRana1286 <tarunrana237h@gmail.com>
2026-02-18 00:07:40 +0530

    Main for User

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -9,6 +9,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import { Route } from 'react-router-dom'
+import User from './components/User/User.jsx'
 // const router = createBrowserRouter([
 //   { 
 //     path: '/',
@@ -27,6 +28,7 @@ const router = createBrowserRouter(
       <Route index element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
+      <Route path='user/:userId' element={<User />} />
     </Route>
   )
 )
