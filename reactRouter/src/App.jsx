b6756933f8aba15cdc747403ffaac4885bd5a4f7
TarunRana1286 <tarunrana237h@gmail.com>
2026-02-17 09:13:39 +0530

    Css Applied

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import './index.css'
 import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>React Router App</h1>
+      <h1 className="text-3xl font-bold text-center text-blue-500">React Router App</h1>
     </>
   )
 }
