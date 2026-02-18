644bf3439edd84a6d94c4b5619b5db3dbd01bcd7
TarunRana1286 <tarunrana237h@gmail.com>
2026-02-18 09:28:37 +0530

    Changes for User file

--- reactRouter/src/components/User/User.jsx
+++ reactRouter/src/components/User/User.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom' 
 
 function User() {
+  const { userId } = useParams()
   return (
-    <div>User</div>
+    <div className='bg-gray-600 text-white text-3xl'>User : {userId}</div>
   )
 }
 
