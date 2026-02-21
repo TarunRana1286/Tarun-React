1d6afaa2ed4cb0ad60e7d2c92a33a9e27eee3eab
TarunRana1286 <tarunrana237h@gmail.com>
2026-02-21 22:54:45 +0530

    Complete Context API Project

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -4,15 +4,18 @@ function Login() {
     const [user,setUser] = useState('');
     const [password,setPassword] = useState('');
 
+    const {setUser: setUserContext} = useContext(UserContext);
+
     const handlesubmit = (e) => {
         e.preventDefault();
+        setUserContext(user);
     }
   return (
     <div>
         <h1>Login Page</h1>
         <input type="text" placeholder='Username' value={user} onChange={(e) => setUser(e.target.value)}/>
-        <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-        <button>Login</button>
+{" "}        <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <button onClick={handlesubmit}>Login</button>
     </div>
   )
 }
