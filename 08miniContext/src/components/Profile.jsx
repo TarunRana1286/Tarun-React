a5608e881540f82172c790729638a68d2d4d17db
TarunRana1286 <tarunrana237h@gmail.com>
2026-02-21 22:51:51 +0530

    Profile Context

---
+++ 08miniContext/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React,{useContext} from 'react'
+import UserContext from '../context/UserContext'
+function Profile() {
+    const {user} = useContext(UserContext);
+  if(!user){
+    return <h1>Please Login to view profile</h1>
+  }
+    return <h1>Welcome {user}</h1>
+}
+
+export default Profile
