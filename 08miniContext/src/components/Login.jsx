6e1a66d0863053c532948b2d322a79976d7ab30a
TarunRana1286 <tarunrana237h@gmail.com>
2026-02-21 22:42:12 +0530

    Need to add more code

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -6,11 +6,12 @@ function Login() {
 
     const handlesubmit = (e) => {
         e.preventDefault();
+    }
   return (
     <div>
         <h1>Login Page</h1>
-        <input type="text" placeholder='Username'/>
-        <input type="password" placeholder='Password'/>
+        <input type="text" placeholder='Username' value={user} onChange={(e) => setUser(e.target.value)}/>
+        <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
         <button>Login</button>
     </div>
   )
