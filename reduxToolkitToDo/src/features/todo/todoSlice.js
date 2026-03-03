2ec563426ba02f92f002b1bd8a260e1c4e7233ed
TarunRana1286 <tarunrana237h@gmail.com>
2026-03-03 23:33:18 +0530

    Import

--- reduxToolkitToDo/src/features/todo/todoSlice.js
+++ reduxToolkitToDo/src/features/todo/todoSlice.js
@@ -1,2 +1,9 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
+
+const initialState = {
+  todos: [
+    { id: nanoid(), title: "Learn Redux Toolkit", completed: false },
+    { id: nanoid(), title: "Build a to-do app", completed: false },
+  ],
+};
