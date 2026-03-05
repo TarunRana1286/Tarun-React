38a8ea4b05d0a05fc5a486474c6e2427090b2059
TarunRana1286 <tarunrana237h@gmail.com>
2026-03-05 23:46:56 +0530

    Update and Delete ToDo

--- reduxToolkitToDo/src/features/todo/todoSlice.js
+++ reduxToolkitToDo/src/features/todo/todoSlice.js
@@ -22,5 +22,21 @@ export const todoSlice = createSlice({
     removeTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
+    updateTodo: (state, action) => {
+      const { id, title } = action.payload;
+      const existingTodo = state.todos.find((todo) => todo.id === id);
+      if (existingTodo) {
+        existingTodo.title = title;
+      }
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+   toggleTodo: (state, action) => {
+      const existingTodo = state.todos.find((todo) => todo.id === action.payload);
+      if (existingTodo) {
+        existingTodo.completed = !existingTodo.completed;
+      }
+    },
   }
 });
